e4a003a9d5b76c848dbe4eeb2a757a3442c8c6d7
Koppenhafer, Sean D <sean.d.koppenhafer@intel.com>
2017-02-09 15:21:42 -0800

    Added trip database generation. City generation still needs to be added at home

--- gen_database/gen_database.py
+++ gen_database/gen_database.py
@@ -8,7 +8,10 @@ def main():
     last_names = get_list_of_names_from_file("last_names.txt")
 
     employee_database = generate_employee_database(first_names, last_names)
-    print_employee_database(employee_database, "employee_info.db")
+    write_employee_database(employee_database, "employee_info.db")
+
+    trips_database = generate_trip_database(employee_database, 100000)
+    write_trip_database(trips_database, "trips.db")
 
 
 def get_list_of_names_from_file(filename):
@@ -26,16 +29,16 @@ def extract_list_of_names(text_stream):
     return names_list
 
 
-def get_random_number(max_bound):
-    return random.randrange(0, max_bound)
+def get_random_number(min_bound, max_bound):
+    return random.randrange(min_bound, max_bound)
 
 
 def generate_random_name(first_names, last_names):
     number_of_first_names = len(first_names)
     number_of_last_names = len(last_names)
 
-    first_name_index = get_random_number(number_of_first_names)
-    last_name_index = get_random_number(number_of_last_names)
+    first_name_index = get_random_number(0, number_of_first_names)
+    last_name_index = get_random_number(0, number_of_last_names)
 
     name = first_names[first_name_index] + ' ' + last_names[last_name_index]
     return name
@@ -53,10 +56,33 @@ def generate_employee_database(first_names, last_names):
     return employees
 
 
-def print_employee_database(employee_database, output_filename):
+def write_employee_database(employee_database, output_filename):
     with open(output_filename, 'w') as output_filehandle:
         for employee in employee_database:
             output_filehandle.write("{},{}\n".format(employee['ID'], employee['name']))
+
+
+def generate_trip_database(employee_database, num_of_trips):
+    number_of_employees = len(employee_database)
+    trips = list()
+
+    for i in range(0, num_of_trips):
+        trip = dict()
+        date = str(get_random_number(1, 12)).zfill(2) + '/' + str(get_random_number(1, 30)).zfill(2) + '/' + str(get_random_number(1, 17)).zfill(2)
+        employee = employee_database[get_random_number(0, number_of_employees)]
+        trip['date'] = date
+        trip['ID'] = employee['ID']
+        trip['city'] = "placeholder"
+        trips.append(trip)
+
+    return trips
+
+
+def write_trip_database(trip_database, output_filename):
+    with open(output_filename, 'w') as output_filehandle:
+        for trip in trip_database:
+            output_filehandle.write("{},{},{}\n".format(trip['date'], trip['ID'], trip['city']))
+
 
 if __name__ == '__main__':
     main()
